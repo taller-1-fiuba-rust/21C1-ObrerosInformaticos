b30e9accde76b2edf3fd7fd957a31a8601d2f023
Maximiliano Levi <maxilevi77@gmail.com>
2021-05-20 18:07:37 -0300

    add tests for protocol types

--- src/protocol/types.rs
+++ src/protocol/types.rs
@@ -44,5 +44,62 @@ impl ProtocolType {
 
 #[cfg(test)]
 mod tests {
+    use crate::protocol::types::ProtocolType;
 
+    #[test]
+    fn test_get_integer() {
+        let val = ProtocolType::Integer(10);
+        assert_eq!(val.integer(), 10);
+    }
+
+    #[test]
+    fn test_get_negative_integer() {
+        let val = ProtocolType::Integer(-10);
+        assert_eq!(val.integer(), -10);
+    }
+
+    #[test]
+    fn test_get_string() {
+        let val = ProtocolType::String("Hi!".to_string());
+        assert_eq!(val.string(), "Hi!");
+    }
+
+    #[test]
+    fn test_get_array() {
+        let val = ProtocolType::Array(vec![ProtocolType::Integer(10), ProtocolType::String("Hi!".to_string())]);
+        let arr = val.array();
+        assert_eq!(arr.len(), 2);
+        assert_eq!(arr[0].integer(), 10);
+        assert_eq!(arr[1].clone().string(), "Hi!");
+    }
+
+    #[test]
+    fn test_serialize_integer() {
+        let val = ProtocolType::Integer(10);
+        assert_eq!(val.serialize(), ":10\r\n");
+    }
+
+    #[test]
+    fn test_serialize_negative_integer() {
+        let val = ProtocolType::Integer(-10);
+        assert_eq!(val.serialize(), ":-10\r\n");
+    }
+
+    #[test]
+    fn test_serialize_string() {
+        let val = ProtocolType::String("Hi!".to_string());
+        assert_eq!(val.serialize(), "$3\r\nHi!\r\n");
+    }
+
+    #[test]
+    fn test_serialize_array() {
+        let val = ProtocolType::Array(vec![ProtocolType::Integer(10), ProtocolType::String("Hi!".to_string())]);
+        assert_eq!(val.serialize(), "*2\r\n:10\r\n$3\r\nHi!\r\n");
+    }
+
+    #[test]
+    fn test_serialize_nested_array() {
+        let val = ProtocolType::Array(vec![ProtocolType::Integer(10), ProtocolType::Array(vec![ProtocolType::Integer(4), ProtocolType::Integer(3)]), ProtocolType::Integer(1)]);
+        assert_eq!(val.serialize(), "*3\r\n:10\r\n*2\r\n:4\r\n:3\r\n:1\r\n");
+    }
 }
