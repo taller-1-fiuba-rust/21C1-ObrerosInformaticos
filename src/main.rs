d6bc8a412e712e50b0daf64b2a17deff0297f790
Andres Jalife <51293183+AndresJalife@users.noreply.github.com>
2021-05-19 21:32:42 -0300

    Revert "[Closes #1] Arquitectura"

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-mod listener_thread;
-mod server;
-mod threadpool;
-
 fn main() {
-    let addr = "127.0.0.1:1234".to_string();
-    let mut server = server::Server::new(addr);
-    server.run();
-    server.join();
+    println!("Hello, world!");
 }
