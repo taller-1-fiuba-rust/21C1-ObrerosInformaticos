c0040930c6d0240534ce656e74f08fd59b0e5faa
Maximiliano Levi <maxilevi77@gmail.com>
2021-05-20 17:11:40 -0300

    add code for parsing bulk strings

--- src/types.rs
+++ src/types.rs
@@ -143,6 +143,44 @@ impl ProtocolParser for ArrayParser {
     }
 }
 
+struct BulkStringParser {
+    data: String,
+    length: usize
+}
+
+impl BulkStringParser {
+    pub fn new() -> Self {
+        BulkStringParser {
+            data: String::new(),
+            length: 0
+        }
+    }
+}
+
+impl ProtocolParser for BulkStringParser {
+    fn get_prefix(&self) -> char {
+        return '$';
+    }
+
+    fn feed(&mut self, line: &String) -> bool {
+        let len = line.len();
+        assert!(len > 0);
+        let symbol = line.chars().nth(0).unwrap();
+        return if symbol == self.get_prefix() {
+            self.length = line[1..len-2].parse().unwrap();
+            false
+        } else {
+            self.data = line[0..self.length].to_string();
+            true
+        }
+    }
+
+    fn build(&self) -> ProtocolType {
+        assert_eq!(self.length, self.data.len());
+        return ProtocolType::String(self.data.clone());
+    }
+}
+
 struct ParserFactory;
 
 impl ParserFactory {
