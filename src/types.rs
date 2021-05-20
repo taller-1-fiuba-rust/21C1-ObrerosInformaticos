8152b348915769422da9b53d1eae6775e9d6c1d4
Maximiliano Levi <maxilevi77@gmail.com>
2021-05-20 17:11:40 -0300

    add new tests for arrays

--- src/types.rs
+++ src/types.rs
@@ -283,4 +283,38 @@ mod tests {
         let nested_arr = result[1].clone().array();
         assert_eq!(nested_arr[0].integer(), 4);
     }
+
+    #[test]
+    fn parse_empty_array() {
+        let lines = vec!["*0\r\n"];
+        let result = parse_array(lines);
+
+        assert_eq!(result.len(), 0);
+    }
+
+    fn split_lines(string: &str) -> Vec<&str> {
+        let mut vector: Vec<&str> = Vec::new();
+        let chars: Vec<char> = string.chars().collect();
+        let mut i = 0;
+        let mut offset = 0;
+        let len = chars.len();
+        while i < len {
+            if chars[i] == '\n' {
+                vector.push(&string[offset..i+1]);
+                offset = i+1;
+            }
+            i += 1;
+        }
+        vector
+    }
+
+    #[test]
+    fn parse_array_with_bulk_strings() {
+        let lines = split_lines("*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n");
+        let result = parse_array(lines);
+
+        assert_eq!(result.len(), 2);
+        assert_eq!(result[0].clone().string(), "foo");
+        assert_eq!(result[1].clone().string(), "bar");
+    }
 }
