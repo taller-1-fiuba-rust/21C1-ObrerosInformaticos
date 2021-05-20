0ddde3f3d1651de096974bc0ee8df0c5bad68e2f
Maximiliano Levi <maxilevi77@gmail.com>
2021-05-20 20:37:13 -0300

    use correct redis port

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod server;
 mod threadpool;
 
 fn main() {
-    let addr = "127.0.0.1:1234".to_string();
+    let addr = "127.0.0.1:6379".to_string();
     let mut server = server::Server::new(addr);
     server.run();
     server.join();
