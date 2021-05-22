86a430fedc99d13a9e94ec1a33bfaecb453dd700
Andres Jalife <ajalife@odeasrl.com.ar>
2021-05-22 12:06:28 -0300

    problemas con lifetime

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,23 @@ mod configuration;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let configuration = configuration::Configuration::new(&args[1]);
+    if args.len() < 2{
+        println!("Faltan parametros de inicio");
+        return;
+    }
+    let mut configuration = configuration::Configuration::new();
+    match configuration.set_config(&args[1]) {
+        Err(_) => {
+            // como saber si x es un tipo u otro de error
+            println!("Faltan datos en el archivo de configuración.");
+            return;
+        }
+        Ok(_) => (),
+    }
+
+    println!("{}", configuration.get_verbose());
+    println!("{}", configuration.get_dbfilename());
+    println!("{}", configuration.get_logfile());
+    println!("{}", configuration.get_port());
+    println!("{}", configuration.get_timeout());
 }
