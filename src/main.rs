ebcab8101b26ee95f6505d5bf006d45ad24f730c
Andres Jalife <ajalife@odeasrl.com.ar>
2021-05-23 19:34:39 -0300

    merge

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
 use std::env;
 mod configuration;
-
+mod execution;
+mod listener_thread;
+mod protocol;
+mod server;
+mod storage;
+mod threadpool;
 use crate::configuration::configuration::Configuration;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Faltan parametros de inicio");
-        return;
-    }
-
     let mut configuration = Configuration::new();
 
     if let Err(msj) = configuration.set_config(&args[1]) {
         println!("{}", msj);
         return;
     }
+
+    let addr = "127.0.0.1:6379".to_string();
+    let mut server = server::Server::new(addr);
+    server.run();
+    server.join();
 }
