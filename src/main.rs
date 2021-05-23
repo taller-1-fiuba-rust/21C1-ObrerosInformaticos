be2faa0324fd90b18d5a0fc4ea7311eeda725651
Andres Jalife <ajalife@odeasrl.com.ar>
2021-05-23 11:42:33 -0300

    termino configuration

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,21 @@
 use std::env;
 mod configuration;
 
+use crate::configuration::configuration::Configuration;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2{
+    if args.len() < 2 {
         println!("Faltan parametros de inicio");
         return;
     }
-    let mut configuration = configuration::Configuration::new();
+    
+    let mut configuration = Configuration::new();
     match configuration.set_config(&args[1]) {
-        Err(_) => {
-            // como saber si x es un tipo u otro de error
-            println!("Faltan datos en el archivo de configuración.");
+        Err(msj) => {
+            println!("{}",msj);
             return;
         }
         Ok(_) => (),
     }
-
-    println!("{}", configuration.get_verbose());
-    println!("{}", configuration.get_dbfilename());
-    println!("{}", configuration.get_logfile());
-    println!("{}", configuration.get_port());
-    println!("{}", configuration.get_timeout());
 }
