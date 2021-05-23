3c350060552451ef36027e42fa98ae2ca77aa193
Andres Jalife <ajalife@odeasrl.com.ar>
2021-05-23 12:02:37 -0300

    Delete unnecesary files

--- target/debug/deps/proyecto_taller_1-8d15f7356f91462c.d
+++
@@ -1,7 +0,0 @@
-/home/ajalife/Desktop/Facultad/Materias/Taller De Programacion/redis/Obrero/target/debug/deps/proyecto_taller_1-8d15f7356f91462c.rmeta: src/main.rs src/configuration/mod.rs src/configuration/configuration.rs
-
-/home/ajalife/Desktop/Facultad/Materias/Taller De Programacion/redis/Obrero/target/debug/deps/proyecto_taller_1-8d15f7356f91462c.d: src/main.rs src/configuration/mod.rs src/configuration/configuration.rs
-
-src/main.rs:
-src/configuration/mod.rs:
-src/configuration/configuration.rs:
