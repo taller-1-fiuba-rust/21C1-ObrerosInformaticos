a29423f728f556b5c9df7fc7b059653bada8d0a9
Maximiliano Levi <maxilevi77@gmail.com>
2021-05-27 15:25:15 -0300

    fix errors

--- src/pubsub.rs
+++ src/pubsub.rs
@@ -1,10 +1,10 @@
 use std::collections::{HashMap, HashSet};
-use std::io::prelude::*;
 use std::net::TcpStream;
-use std::sync::{RwLock, Mutex, Arc};
+use std::sync::{Mutex, Arc};
 use crate::protocol::response::ResponseBuilder;
 use crate::protocol::types::ProtocolType;
 use std::sync::atomic::AtomicU32;
+use std::io::Write;
 
 struct Subscriber {
     socket: Arc<Mutex<TcpStream>>,
@@ -16,6 +16,17 @@ impl Subscriber {
         Subscriber {
             socket,
             channel_count: 0
+        }
+    }
+
+    fn send(&mut self, msg: &String) -> Result<(), &'static str> {
+        let mut locked_socket = match self.socket.lock() {
+            Ok(t) => t,
+            Err(e) => return Err("Failed to lock socket")
+        };
+        match locked_socket.write_all(msg.as_bytes()) {
+            Ok(t) => Ok(t),
+            Err(e) => Err(&format!("Error '{}' while writing to socket", e.to_string())[..])
         }
     }
 }
@@ -38,12 +49,12 @@ impl PublisherSubscriber {
     pub fn subscribe(&mut self, socket: Arc<Mutex<TcpStream>>, channel: &String) {
         let key = last_id;
         match self.subscriber_ids.get(&key) {
-            Ok(_) => {}
-            Err(_) => {
-                subscriber_ids.insert(key, Subscriber::new(socket));
+            Some(_) => {}
+            None => {
+                self.subscriber_ids.insert(key, Subscriber::new(socket));
             }
         }
-        *self.subscriptions.entry(channel.clone()).or_insert(HashSet::new()).push(key);
+        self.subscriptions.entry(channel.clone()).or_insert(HashSet::new()).insert(key);
     }
 
     pub fn publish(&mut self, message: String, channel: String) -> i32 {
@@ -52,17 +63,17 @@ impl PublisherSubscriber {
         let response_str = response.serialize();
         let mut count = 0;
 
-        if let Ok(streams) = self.subscriptions.get(&channel) {
-            streams.retain(|subscriber| {
-               match subscriber.write_all(response_str.as_bytes()) {
+        if let Some(mut streams) = self.subscriptions.get(&channel) {
+            streams.retain(|subscriber_id| {
+                let mut subscriber = self.subscriber_ids.get(subscriber_id).unwrap();
+                match subscriber.send(&response_str) {
                    Ok(_) => {
                        count += 1;
                        true
                    },
                    Err(_) => false,
-               }
+                }
             });
-            self.subscriptions.insert(channel, streams);
         }
         count
     }
