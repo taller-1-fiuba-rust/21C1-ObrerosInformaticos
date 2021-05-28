8fb30dde223193371b5a64cebf54cfa0b3f35f46
Maximiliano Levi <maxilevi77@gmail.com>
2021-05-27 21:58:49 -0300

    fix msg and channel inverted

--- src/server_command/pubsub/publish.rs
+++ src/server_command/pubsub/publish.rs
@@ -6,27 +6,27 @@ use std::sync::{Mutex, Arc};
 pub fn run(pubsub: Arc<Mutex<PublisherSubscriber>>, builder: &mut ResponseBuilder, arguments: Vec<ProtocolType>) -> Result<(), String> {
     assert_eq!(arguments.len(), 2);
 
-    let msg = match arguments[0].clone().string() {
+    let channel = match arguments[0].clone().string() {
         Ok(s) => s,
         Err(s) => {
-            return Err(format!("Error '{}' while parsing message'{}'", s, arguments[0].to_string()));
+            return Err(format!("Error '{}' while parsing channel'{}'", s, arguments[0].to_string()));
         }
     };
 
-    let channel = match arguments[1].clone().string() {
+    let msg = match arguments[1].clone().string() {
       Ok(s) => s,
       Err(s) => {
-          return Err(format!("Error '{}' while parsing channel'{}'", s, arguments[1].to_string()));
+          return Err(format!("Error '{}' while parsing message'{}'", s, arguments[1].to_string()));
       }
     };
 
     let mut locked_pubsub = match pubsub.lock() {
-        Err(e) => {
+        Err(_) => {
             return Err("Failed to execute publish".to_string());
         },
         Ok(t) => t
     };
 
-    builder.add(ProtocolType::Integer(locked_pubsub.publish(msg, channel) as i32));
+    builder.add(ProtocolType::Integer(locked_pubsub.publish( channel, msg) as i32));
     Ok(())
 }
