f13e6cd63f92f7b15386ce45617c1bd3ec34bb86
Maximiliano Levi <maxilevi77@gmail.com>
2021-06-02 20:31:16 -0300

    fix tests

--- src/key_command/copy.rs
+++ src/key_command/copy.rs
@@ -14,6 +14,7 @@ pub fn run(db: Arc<DataStorage>, arguments: Vec<ProtocolType>, builder: &mut Res
     let mut result = 0;
     if value.is_some() {
         let new_val = (&value.unwrap().1).clone();
+        drop(read_lock);
         db.add_key_value(&dst, new_val);
         result = 1;
     }
@@ -30,15 +31,16 @@ mod tests {
 
     #[test]
     fn test_copy() {
+
         let data = Arc::new(DataStorage::new());
         let mut builder = ResponseBuilder::new();
         data.add_key_value("key", Value::String("value".to_string()));
 
-        run(data, vec![ProtocolType::String("key".to_string()), ProtocolType::String("new_key".to_string())], &mut builder);
+        run(data.clone(), vec![ProtocolType::String("key".to_string()), ProtocolType::String("new_key".to_string())], &mut builder).unwrap();
 
         let lock = data.read();
-        assert_eq!(data.get("new_key").string().unwrap(), "hola");
-        assert_eq!(builder.serialize(), ":1\r\n");
+        assert_eq!(lock.get("new_key").unwrap().1.string().unwrap(), "value");
+        assert_eq!(builder.serialize(), "*1\r\n:1\r\n");
     }
 
     #[test]
@@ -46,8 +48,8 @@ mod tests {
         let data = Arc::new(DataStorage::new());
         let mut builder = ResponseBuilder::new();
 
-        run(data, vec![ProtocolType::String("no_such_key".to_string()), ProtocolType::String("new_key".to_string())], &mut builder);
+        run(data, vec![ProtocolType::String("no_such_key".to_string()), ProtocolType::String("new_key".to_string())], &mut builder).unwrap();
 
-        assert_eq!(builder.serialize(), ":0\r\n");
+        assert_eq!(builder.serialize(), "*1\r\n:0\r\n");
     }
 }
