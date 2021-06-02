9b8b19ae2b32ce0af9f7b5d040a8a77018cf44cb
Maximiliano Levi <maxilevi77@gmail.com>
2021-06-02 19:55:40 -0300

    add copy tests

--- src/key_command/copy.rs
+++ src/key_command/copy.rs
@@ -21,3 +21,33 @@ pub fn run(db: Arc<DataStorage>, arguments: Vec<ProtocolType>, builder: &mut Res
     builder.add(ProtocolType::Integer(result));
     Ok(())
 }
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+    use crate::storage::data_storage::Value;
+
+    #[test]
+    fn test_copy() {
+        let data = Arc::new(DataStorage::new());
+        let mut builder = ResponseBuilder::new();
+        data.add_key_value("key", Value::String("value".to_string()));
+
+        run(data, vec![ProtocolType::String("key".to_string()), ProtocolType::String("new_key".to_string())], &mut builder);
+
+        let lock = data.read();
+        assert_eq!(data.get("new_key").string().unwrap(), "hola");
+        assert_eq!(builder.serialize(), ":1\r\n");
+    }
+
+    #[test]
+    fn test_copy_with_empty_element() {
+        let data = Arc::new(DataStorage::new());
+        let mut builder = ResponseBuilder::new();
+
+        run(data, vec![ProtocolType::String("no_such_key".to_string()), ProtocolType::String("new_key".to_string())], &mut builder);
+
+        assert_eq!(builder.serialize(), ":0\r\n");
+    }
+}
