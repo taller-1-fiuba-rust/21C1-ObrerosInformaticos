76fcd753d613954f19c80ea7f88d499155d43622
Maximiliano Levi <maxilevi77@gmail.com>
2021-06-02 19:31:23 -0300

    add starting code for copy command

--- src/execution.rs
+++ src/execution.rs
@@ -1,5 +1,6 @@
 use crate::config::configuration::Configuration;
 use crate::key_command::expire;
+use crate::key_command::copy;
 use crate::protocol::command::Command;
 use crate::protocol::response::ResponseBuilder;
 use crate::pubsub::PublisherSubscriber;
@@ -42,6 +43,7 @@ impl Execution {
             "ping" => ping::run(builder),
             "info" => info::run(builder, &self.config, &self.sys_time),
             "expire" => expire::set_expiration_to_key(builder, cmd, &self.data),
+            "copy" => copy::run(self.data.clone(), cmd.arguments(), builder),
             "config" => config::run(cmd.arguments(), builder, self.config.clone()),
             _ => Err("Unknown command."),
         }
---
+++ src/key_command/copy.rs
@@ -0,0 +1,27 @@
+use crate::protocol::response::ResponseBuilder;
+use crate::protocol::types::ProtocolType;
+use std::sync::Arc;
+use crate::storage::data_storage::DataStorage;
+
+pub fn run(db: Arc<DataStorage>, arguments: Vec<ProtocolType>, builder: &mut ResponseBuilder) -> Result<(), &'static str>  {
+    assert_eq!(arguments.len(), 2);
+
+    let src = arguments[0].clone().string()?;
+    let dst = arguments[1].clone().string()?;
+
+    let read_lock = db.read();
+    let value = read_lock.get(&src);
+    let mut result = 0;
+    if value.is_some() {
+        let val = &value.unwrap().1;
+        let new_val = match val {
+            Value::String(s) => s.clone(),
+            
+        }
+        db.add_key_value(&dst, );
+        result = 1;
+    }
+
+    builder.add(ProtocolType::Integer(result));
+    Ok(())
+}
