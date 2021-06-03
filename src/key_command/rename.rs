59c85c5528b18d88bc9650bc506f2961b2e9cd58
Maximiliano Levi <maxilevi77@gmail.com>
2021-06-03 15:53:42 -0300

    add tests for rename command

--- src/key_command/rename.rs
+++ src/key_command/rename.rs
@@ -17,3 +17,47 @@ pub fn run(
     builder.add(ProtocolType::SimpleString("OK".to_string()));
     Ok(())
 }
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+    use crate::storage::data_storage::Value;
+
+    #[test]
+    fn test_rename() {
+        let data = Arc::new(DataStorage::new());
+        let mut builder = ResponseBuilder::new();
+        data.add_key_value("key", Value::String("value".to_string()));
+
+        run(
+            data.clone(),
+            vec![
+                ProtocolType::String("key".to_string()),
+                ProtocolType::String("new_key".to_string()),
+            ],
+            &mut builder,
+        )
+            .unwrap();
+
+        assert_eq!(data.get("new_key").unwrap().string().unwrap(), "value");
+        assert!(data.get("key").is_none());
+        assert_eq!(builder.serialize(), "*1\r\n+OK\r\n");
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_rename_error() {
+        let data = Arc::new(DataStorage::new());
+        let mut builder = ResponseBuilder::new();
+
+        run(
+            data,
+            vec![
+                ProtocolType::String("no_such_key".to_string()),
+                ProtocolType::String("new_key".to_string()),
+            ],
+            &mut builder,
+        ).unwrap();
+    }
+}
