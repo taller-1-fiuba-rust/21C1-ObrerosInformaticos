01d21e73f53884ea89030a220c4176eb695fef62
Andres Jalife <ajalife@odeasrl.com.ar>
2021-06-03 20:10:57 -0300

    finish type command

--- src/key_command/key_type.rs
+++ src/key_command/key_type.rs
@@ -1,7 +1,7 @@
-use crate::protocol::command::Command;
 use crate::protocol::response::ResponseBuilder;
 use crate::protocol::types::ProtocolType;
 use crate::storage::data_storage::DataStorage;
+use crate::storage::data_storage::Value;
 use std::sync::Arc;
 
 pub fn run(
@@ -10,11 +10,21 @@ pub fn run(
     data: &Arc<DataStorage>,
 ) -> Result<(), &'static str> {
     if arguments.len() != 1 {
-        return Err("Wrong quantity of arguments. Command TYPE has only one.")
+        return Err("Wrong quantity of arguments. Command TYPE has only one.");
     }
 
-    let key = arguments[0].clone().string();
+    let key = arguments[0].clone().string()?;
 
-    println!("{}", key.unwrap());
+    let value_option = data.get(&key);
+
+    if let Some(value) = value_option {
+        match value {
+            Value::String(_) => builder.add(ProtocolType::String("String".to_string())),
+            Value::Vec(_) => builder.add(ProtocolType::String("Vec".to_string())),
+            Value::HashSet(_) => builder.add(ProtocolType::String("HashSet".to_string())),
+        }
+    } else {
+        return Err("There's no value for that key");
+    }
     Ok(())
 }
