535f84cae16b67c7a46d3900dbad63e78053caeb
danielagenaro98 <danielagenaro98@gmail.com>
2021-06-06 09:27:13 -0300

    Soluciono error de tipos

--- src/execution.rs
+++ src/execution.rs
@@ -1,5 +1,7 @@
 use crate::config::configuration::Configuration;
-use crate::key_command::{copy, del, exists, expire, key_type, keys, persist, rename, sort, ttl, expireat};
+use crate::key_command::{
+    copy, del, exists, expire, expireat, key_type, keys, persist, rename, sort, ttl,
+};
 use crate::protocol::command::Command;
 use crate::protocol::response::ResponseBuilder;
 use crate::pubsub::PublisherSubscriber;
