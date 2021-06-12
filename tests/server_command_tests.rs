c3e19385c8938f6a3670492e800c423b75d05105
Maximiliano Levi <maxilevi77@gmail.com>
2021-06-12 13:59:28 -0300

    add ping test :mail:

---
+++ tests/server_command_tests.rs
@@ -0,0 +1,8 @@
+mod common;
+
+#[test]
+fn test_ping() {
+    let (_server, client) = common::setup();
+    let result : String = common::query_string(&client, "PING");
+    assert_eq!(result, "PONG");
+}
