c37dd5c3b62a65ca372619dbab17d5c185a805d7
Maximiliano Levi <maxilevi77@gmail.com>
2021-06-12 13:49:03 -0300

    add another abstraction to run strings as redis commands

--- tests/common.rs
+++ tests/common.rs
@@ -2,7 +2,7 @@ use proyecto_taller_1::server::Server;
 use proyecto_taller_1::config::configuration::Configuration;
 use redis::{Client, FromRedisValue};
 
-const PORT: u16 = 12345;
+const PORT: u16 = 10005;
 
 pub fn setup() -> (Server, Client) {
     let mut config = Configuration::new();
@@ -13,7 +13,12 @@ pub fn setup() -> (Server, Client) {
     return (sv, client);
 }
 
-pub fn query<T: FromRedisValue>(client: & Client, cmd: &'static str, args: &[&str]) -> T {
+pub fn query_string<T: FromRedisValue>(client: &Client, cmd: &str) -> T {
+    let args: Vec<&str> = cmd.split(" ").collect();
+    query(client, args[0], &args[1..])
+}
+
+pub fn query<T: FromRedisValue>(client: &Client, cmd: &str, args: &[&str]) -> T {
     let mut cmd = redis::cmd(cmd);
     for arg in args {
         cmd.arg(*arg);
--- tests/key_command_tests.rs
+++ tests/key_command_tests.rs
@@ -3,16 +3,16 @@ mod common;
 #[test]
 fn test_set() {
     let (_server, client) = common::setup();
-    let _ : () = common::query(&client, "set", &["my_key", "42"]);
-    let val : i32 = common::query(&client, "get", &["my_key"]);
+    let _ : () = common::query_string(&client, "SET my_key 42");
+    let val : i32 = common::query_string(&client, "GET my_key");
     assert_eq!(val, 42);
 }
 
 #[test]
 fn test_copy() {
     let (_server, client) = common::setup();
-    let _ : () = common::query(&client, "set", &["my_key", "Hola"]);
-    let _ : () = common::query(&client, "copy", &["my_key", "clone"]);
-    let val : String = common::query(&client, "get", &["clone"]);
+    let _ : () = common::query_string(&client, "SET my_key hola");
+    let _ : () = common::query_string(&client, "COPY my_key clone");
+    let val : String = common::query_string(&client, "GET clone");
     assert_eq!(val, "Hola");
 }
