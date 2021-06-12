826895404c84fab30f8b3cb986629d6e93b3ce32
Maximiliano Levi <maxilevi77@gmail.com>
2021-06-11 21:20:49 -0300

    add a new test :dog:

--- tests/key_command_tests.rs
+++ tests/key_command_tests.rs
@@ -9,4 +9,12 @@ fn test_set() {
     let val : i32 = common::query(&client, "get", &["my_key"]);
     assert_eq!(val, 42);
 }
+
+#[test]
+fn test_copy() {
+    let (_, mut client) = common::setup();
+    let _ : () = common::query(&client, "set", &["my_key", "Hola"]);
+    let _ : () = common::query(&client, "copy", &["my_key", "clone"]);
+    let val : String = common::query(&client, "get", &["clone"]);
+    assert_eq!(val, "Hola");
 }
