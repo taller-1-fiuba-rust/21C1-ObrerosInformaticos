5e54f202323883c1d98d1dbf42fe014558c8acf6
Maximiliano Levi <maxilevi77@gmail.com>
2021-06-11 21:52:41 -0300

    remove muts :chicken:

--- tests/key_command_tests.rs
+++ tests/key_command_tests.rs
@@ -1,10 +1,8 @@
-use redis::Commands;
-
 mod common;
 
 #[test]
 fn test_set() {
-    let (_, mut client) = common::setup();
+    let (_, client) = common::setup();
     let _ : () = common::query(&client, "set", &["my_key", "42"]);
     let val : i32 = common::query(&client, "get", &["my_key"]);
     assert_eq!(val, 42);
@@ -12,7 +10,7 @@ fn test_set() {
 
 #[test]
 fn test_copy() {
-    let (_, mut client) = common::setup();
+    let (_, client) = common::setup();
     let _ : () = common::query(&client, "set", &["my_key", "Hola"]);
     let _ : () = common::query(&client, "copy", &["my_key", "clone"]);
     let val : String = common::query(&client, "get", &["clone"]);
