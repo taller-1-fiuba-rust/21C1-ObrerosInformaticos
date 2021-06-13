1fbd2d62da18a4a3acc409f40be7c7835af8c025
Andres Jalife <ajalife@odeasrl.com.ar>
2021-06-13 13:08:08 -0300

    tests

--- src/string_command/getset.rs
+++ src/string_command/getset.rs
@@ -20,12 +20,16 @@ pub fn run(
             Value::String(old_value) => {
                 let new_value = arguments[1].clone().string()?;
                 data.set(&key, Value::String(new_value))?;
-                builder.add(ProtocolType::SimpleString(old_value.to_string()));
+                builder.add(ProtocolType::String(old_value));
             }
-            Value::Vec(_) => return Err("The value stored is not a string"),
-            Value::HashSet(_) => return Err("The value stored is not a string"),
+            Value::Vec(_) => {
+                return Err("WRONGTYPE Operation against a key holding the wrong kind of value")
+            }
+            Value::HashSet(_) => {
+                return Err("WRONGTYPE Operation against a key holding the wrong kind of value")
+            }
         },
-        None => builder.add(ProtocolType::SimpleString("nil".to_string())),
+        None => builder.add(ProtocolType::String("nil".to_string())),
     }
     Ok(())
 }
@@ -34,10 +38,95 @@ pub fn run(
 
 mod tests {
     use super::*;
+    use std::collections::HashSet;
 
     #[test]
-    fn test_getset() {
+    fn test_getset_ok() {
         let data = Arc::new(DataStorage::new());
         let mut builder = ResponseBuilder::new();
+
+        data.set("src", Value::String("value".to_string())).unwrap();
+
+        run(
+            &mut builder,
+            vec![
+                ProtocolType::String("src".to_string()),
+                ProtocolType::String("new_value".to_string()),
+            ],
+            &data.clone(),
+        )
+        .unwrap();
+
+        assert_eq!(builder.serialize(), "*1\r\n$5\r\nvalue\r\n");
+        assert_eq!(data.get("src").unwrap().string().unwrap(), "new_value");
+    }
+
+    #[test]
+    fn test_getset_nil() {
+        let data = Arc::new(DataStorage::new());
+        let mut builder = ResponseBuilder::new();
+
+        run(
+            &mut builder,
+            vec![
+                ProtocolType::String("src".to_string()),
+                ProtocolType::String("new_value".to_string()),
+            ],
+            &data.clone(),
+        )
+        .unwrap();
+
+        assert_eq!(builder.serialize(), "*1\r\n$3\r\nnil\r\n");
+    }
+
+    #[test]
+    fn test_getset_err_list() {
+        let data = Arc::new(DataStorage::new());
+        let mut builder = ResponseBuilder::new();
+
+        data.set("src", Value::Vec(vec!["value".to_string()]))
+            .unwrap();
+
+        let result = run(
+            &mut builder,
+            vec![
+                ProtocolType::String("src".to_string()),
+                ProtocolType::String("new_value".to_string()),
+            ],
+            &data.clone(),
+        );
+
+        match result {
+            Ok(_) => assert_eq!(true, false),
+            Err(msg) => assert_eq!(
+                msg,
+                "WRONGTYPE Operation against a key holding the wrong kind of value"
+            ),
+        }
+    }
+
+    #[test]
+    fn test_getset_err_set() {
+        let data = Arc::new(DataStorage::new());
+        let mut builder = ResponseBuilder::new();
+
+        data.set("src", Value::HashSet(HashSet::new())).unwrap();
+
+        let result = run(
+            &mut builder,
+            vec![
+                ProtocolType::String("src".to_string()),
+                ProtocolType::String("new_value".to_string()),
+            ],
+            &data.clone(),
+        );
+
+        match result {
+            Ok(_) => assert_eq!(true, false),
+            Err(msg) => assert_eq!(
+                msg,
+                "WRONGTYPE Operation against a key holding the wrong kind of value"
+            ),
+        }
     }
 }
