d563b52928db1b4cab451da5ee2c701b4483823a
Maximiliano Levi <maxilevi77@gmail.com>
2021-06-15 02:24:40 -0300

    use client in pubsub

--- src/pubsub.rs
+++ src/pubsub.rs
@@ -5,85 +5,79 @@ use std::io::Write;
 use std::net::TcpStream;
 use std::sync::atomic::{AtomicU32, Ordering};
 use std::sync::{Arc, Mutex};
+use crate::client::Client;
 
 /// A pub/sub subscribers. Stores a list of channels and a socket to relay messages to.
 struct Subscriber {
-    socket: Arc<Mutex<TcpStream>>,
-    channels: Vec<String>,
+    channels: HashSet<String>,
+    socket: Arc<Client>
 }
 
 impl Subscriber {
-    fn new(socket: Arc<Mutex<TcpStream>>) -> Self {
+    fn new(client: Arc<Client>) -> Self {
         Subscriber {
-            socket,
-            channels: Vec::new(),
+            socket: client,
+            channels: HashSet::new(),
         }
     }
 
-    fn send(&mut self, msg: &str) -> Result<(), String> {
-        let mut locked_socket = match self.socket.lock() {
-            Ok(t) => t,
-            Err(_) => return Err("Failed to lock socket".to_string()),
-        };
-        match locked_socket.write_all(msg.as_bytes()) {
-            Ok(_) => Ok(()),
-            Err(e) => Err(format!("Error '{}' while writing to socket", e.to_string())),
-        }
+    fn send(&self, msg: &str) -> Result<(), &'static str> {
+        self.socket.send(msg)
     }
 }
 
 /// Struct which holds all the Publisher/Subscriber information. This includes
 /// subscriptions, and clients.
 pub struct PublisherSubscriber {
-    subscriber_ids: HashMap<u32, Subscriber>,
-    subscriptions: HashMap<String, HashSet<u32>>,
-    last_id: AtomicU32,
+    subscriber_ids: HashMap<Arc<Client>, Subscriber>,
+    subscriptions: HashMap<String, HashSet<Arc<Client>>>,
 }
 
 impl PublisherSubscriber {
     pub fn new() -> Self {
         PublisherSubscriber {
             subscriber_ids: HashMap::new(),
-            subscriptions: HashMap::new(),
-            last_id: AtomicU32::new(0),
+            subscriptions: HashMap::new()
         }
     }
 
-    #[allow(dead_code)]
-    ///
-    /// Subscribes a socket to a specific channel, returns the number of channels the socket is subscribed to.
-    ///
-    pub fn subscribe(&mut self, client: Arc<Mutex<TcpStream>>, channel: &str) -> u32 {
-        let client_id = self.last_id.fetch_add(1, Ordering::SeqCst);
+    fn drop_user(&mut self, user: Arc<Client>) {
+        self.subscriber_ids.remove(&user);
+        user.set_pubsub_mode(false);
+    }
+
+    fn add_user(&mut self, user: Arc<Client>) {
         self.subscriber_ids
-            .entry(client_id)
-            .or_insert_with(|| Subscriber::new(client));
+            .entry(user.clone())
+            .or_insert_with(|| Subscriber::new(user.clone()));
+        user.set_pubsub_mode(true);
+    }
+
+    /// Subscribes a socket to a specific channel, returns the number of channels the socket is subscribed to.
+    pub fn subscribe(&mut self, client: Arc<Client>, channel: &str) -> u32 {
+        self.add_user(client.clone());
         self.subscriptions
             .entry(channel.to_string())
             .or_insert_with(HashSet::new)
-            .insert(client_id);
+            .insert(client.clone());
 
-        let sub = self.subscriber_ids.get_mut(&client_id).unwrap();
-        sub.channels.push(channel.to_string());
+        let sub = self.subscriber_ids.get_mut(&client).unwrap();
+        sub.channels.insert(channel.to_string());
         sub.channels.len() as u32
     }
 
-    ///
     /// Publishes a message to a specific channel. Returns the number of subscribers which received the message.
-    ///
     pub fn publish(&mut self, channel: String, message: String) -> u32 {
-        let mut response = ResponseBuilder::new();
-        response.add(ProtocolType::String(message));
-        let response_str = response.serialize();
+        let response_str = Self::build_response(&channel, &message);
         let mut count = 0;
 
-        if let Some(streams) = self.subscriptions.get_mut(&channel) {
+        if let Some(clients) = self.subscriptions.get(&channel) {
             let subs = &mut self.subscriber_ids;
             let count_ref = &mut count;
 
-            let mut dead_users: Vec<u32> = Vec::new();
-            for subscriber_id in streams.iter() {
-                let subscriber = subs.get_mut(subscriber_id).unwrap();
+            let mut dead_users = Vec::new();
+            for client in clients {
+                let subscriber = subs.get(client).unwrap();
                 let result = match subscriber.send(&response_str) {
                     Ok(_) => {
                         *count_ref += 1;
@@ -91,8 +85,9 @@ impl PublisherSubscriber {
                     }
                     Err(_) => false,
                 };
+
                 if !result {
-                    dead_users.push(*subscriber_id);
+                    dead_users.push(client.clone());
                 }
             }
 
@@ -103,14 +98,44 @@ impl PublisherSubscriber {
         count
     }
 
-    ///
+    fn build_response(channel: &String, message: &String) -> String {
+        let mut response = ResponseBuilder::new();
+        response.add(
+            ProtocolType::Array(
+                vec![
+                    ProtocolType::String("message".to_string()),
+                    ProtocolType::String(channel.clone()),
+                    ProtocolType::String(message.clone()),
+                ]
+            )
+        );
+        response.serialize()
+    }
+
+    pub fn get_subscriptions(&self, user: Arc<Client>) -> Vec<String> {
+        let sub = self.subscriber_ids.get(&user).unwrap();
+        sub.channels.iter().map(|x| x.clone()).collect::<Vec<String>>()
+    }
+
     /// Unsubscribes a user from all the channels it's subscribed.
-    ///
-    pub fn unsubscribe(&mut self, user: u32) {
-        let sub = self.subscriber_ids.get_mut(&user).unwrap();
-        for channel in &sub.channels {
+    pub fn unsubscribe_from_channel(&mut self, user: Arc<Client>, channel: &String) -> usize {
+        if let Some(sub) = self.subscriber_ids.get_mut(&user) {
             self.subscriptions.get_mut(channel).unwrap().remove(&user);
+            sub.channels.remove(channel);
+            let len = sub.channels.len();
+            if sub.channels.is_empty() {
+                self.drop_user(user);
+            }
+            len
+        } else {
+            0
         }
-        self.subscriber_ids.remove(&user);
+    }
+
+    /// Unsubscribes a user from all the channels it's subscribed.
+    fn unsubscribe(&mut self, user: Arc<Client>) {
+        for channel in self.get_subscriptions(user.clone()) {
+            self.unsubscribe_from_channel(user.clone(), &channel);
+        }
     }
 }
