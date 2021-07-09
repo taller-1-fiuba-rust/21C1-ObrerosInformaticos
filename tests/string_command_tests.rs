ce602e2db2faa429068f7633c482b0536f96b168
danielagenaro98 <danielagenaro98@gmail.com>
2021-07-09 08:04:58 -0300

    string command test

--- tests/string_command_tests.rs
+++ tests/string_command_tests.rs
@@ -1,6 +1,62 @@
 mod common;
 
 #[test]
+fn test_append() {
+    let (_server, client) = common::setup();
+    let result: i64 = common::query_string(&client, "EXISTS mykey");
+    assert_eq!(result, 0);
+    let val1: i64 = common::query_string(&client, "APPEND mykey Hello");
+    let val2: i64 = common::query_string(&client, "APPEND mykey World");
+    let val3: String = common::query_string(&client, "GET mykey");
+    assert_eq!(val1, 5);
+    assert_eq!(val2, 10);
+    assert_eq!(val3, "HelloWorld");
+}
+
+#[test]
+fn test_decrby() {
+    let (_server, client) = common::setup();
+    let result: String = common::query_string(&client, "SET key1 10");
+    assert_eq!(result, "OK");
+    let result: i64 = common::query_string(&client, "DECRBY key1 3");
+    assert_eq!(result, 7);
+}
+
+
+#[test]
+fn test_getdel() {
+    let (_server, client) = common::setup();
+    let result: String = common::query_string(&client, "SET key1 Hello");
+    assert_eq!(result, "OK");
+    let val: String = common::query_string(&client, "GETDEL key1");
+    assert_eq!(val, "Hello");
+    let result: i64 = common::query_string(&client, "EXISTS key1");
+    assert_eq!(result, 0);
+}
+
+#[test]
+fn test_mget() {
+    let (_server, client) = common::setup();
+    let result: String = common::query_string(&client, "SET key1 Hello");
+    assert_eq!(result, "OK");
+    let result: String = common::query_string(&client, "SET key2 World");
+    assert_eq!(result, "OK");
+    let val: Vec<String> = common::query_string(&client, "MGET key1 key2 key3");
+    assert_eq!(val[0], "Hello");
+    assert_eq!(val[1], "World");
+    assert_eq!(val.len(), 2);
+}
+
+#[test]
+fn test_strlen() {
+    let (_server, client) = common::setup();
+    let result: String = common::query_string(&client, "SET mykey Hello");
+    assert_eq!(result, "OK");
+    let val1: i64 = common::query_string(&client, "STRLEN mykey");
+    assert_eq!(val1, 5);
+}
+
+#[test]
 fn test_incrby_ok() {
     let (_server, client) = common::setup();
     let result: String = common::query_string(&client, "SET my_key 5");
