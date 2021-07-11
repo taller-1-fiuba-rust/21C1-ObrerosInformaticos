3e3ad49bbf9fbdde0ed4f4b72ea6abb84f45eec3
Maximiliano Levi <maxilevi77@gmail.com>
2021-07-11 19:04:46 -0300

    add new integration test

--- tests/integrative_tests.rs
+++ tests/integrative_tests.rs
@@ -62,3 +62,43 @@ fn test_2() {
     assert_eq!(val12, "PONG");
     assert_eq!(val13, 2);
 }
+
+#[test]
+fn test_3() {
+    let (_server, client) = common::setup();
+    let _: () = common::query_string(&client, "MSET first test1 second test2");
+    let val2: String = common::query_string(&client, "GETSET first 123");
+    let val3: String = common::query_string(&client, "GET first");
+    let val4: String = common::query_string(&client, "GET second");
+    let val5: String = common::query_string(&client, "TYPE first");
+    let val6: i64 = common::query_string(&client, "TTL first");
+    let val8: Vec<String> = common::query_string(&client, "KEYS s*");
+    let val9: i64 = common::query_string(&client, "LPUSH third 1 2 3");
+    let val10: i64 = common::query_string(&client, "STRLEN second");
+    let val11: i64 = common::query_string(&client, "STRLEN nada");
+    let val12: i64 = common::query_string(&client, "DEL first");
+    let mut val13: Vec<String> = common::query_string(&client, "KEYS *d");
+    let val14: i64 = common::query_string(&client, "TOUCH first second third");
+    let val16: Vec<String> = common::query_string(&client, "RPOP third 2");
+    let val15: i64 = common::query_string(&client, "LLEN third");
+    let val18: i64 = common::query_string(&client, "RPUSH third 3 2 5 5 6 7 8 9");
+    let val17: Vec<String> = common::query_string(&client, "LRANGE third 2 6");
+
+    assert_eq!(val2, "test1");
+    assert_eq!(val3, "123");
+    assert_eq!(val4, "test2");
+    assert_eq!(val5, "string");
+    assert_eq!(val6, -1);
+    assert_eq!(val8, ["second"]);
+    assert_eq!(val9, 3);
+    assert_eq!(val10, 5);
+    assert_eq!(val11, 0);
+    assert_eq!(val12, 1);
+    val13.sort();
+    assert_eq!(val13, ["second", "third"]);
+    assert_eq!(val14, 2);
+    assert_eq!(val16, ["1", "2"]);
+    assert_eq!(val15, 1);
+    assert_eq!(val18, 9);
+    assert_eq!(val17, ["2", "5", "5", "6", "7"]);
+}
