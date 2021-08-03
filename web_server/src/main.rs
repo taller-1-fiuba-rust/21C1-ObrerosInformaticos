3111175ee4bd4867f1ed41f8c074b854c2dde39c
Maximiliano Levi <maxilevi77@gmail.com>
2021-08-03 14:36:53 -0300

    remove dead code

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -6,20 +6,18 @@ mod server;
 use redis_server::config::configuration::Configuration;
 use redis_server::logging::logger::Logger;
 use redis_server::server::Server;
-use std::sync::atomic::{AtomicU16, Ordering};
 use std::sync::Arc;
 
-static PORT: AtomicU16 = AtomicU16::new(10002);
+const REDIS_PORT: u16 = 10003;
 
 fn main() {
-    let port = PORT.fetch_add(1, Ordering::SeqCst);
     let mut config = Configuration::new();
     let logger: Arc<Logger> = Arc::new(Logger::new(config.get_logfile()).unwrap());
-    config.set_port(port);
+    config.set_port(REDIS_PORT);
     let mut redis_sv = Server::new(config, logger);
     redis_sv.run();
 
-    let mut sv = server::Server::new("localhost", 8080, redis_sv, port);
+    let mut sv = server::Server::new("localhost", 8080, redis_sv, REDIS_PORT);
     sv.run();
     sv.join();
 }
