6ed28cd3a5d29b944bd04dc792562775a9e00b59
Maximiliano Levi <maxilevi77@gmail.com>
2021-08-02 21:53:14 -0300

    read correctly from the redis server

--- web_server/src/client.rs
+++ web_server/src/client.rs
@@ -2,11 +2,14 @@ use std::net::{TcpStream};
 use std::io::{Read, Write};
 use redis_protocol::types::ProtocolType;
 use std::str::from_utf8;
+use std::time::Duration;
+use redis_protocol::parser::ParserFactory;
 
-pub fn send_request(connection_port: String, request: &String) -> Result<String, &'static str> {
+pub fn send_request(connection_port: String, request: &String) -> Result<Vec<u8>, &'static str> {
   
   match TcpStream::connect(connection_port) {
     Ok(mut stream) => {
+
         let vector: Vec<&str> = request.split(" ").collect();
         let mut protocol_vector: Vec<ProtocolType> = vec![];
         for str in vector {
@@ -14,16 +17,43 @@ pub fn send_request(connection_port: String, request: &String) -> Result<String,
         }
         let protocol_string = ProtocolType::Array(protocol_vector).serialize();
 
+        stream
+            .set_read_timeout(Some(Duration::from_millis(10)))
+            .ok()
+            .ok_or("Failed to read from socket")?;
         stream.write(protocol_string.as_bytes()).unwrap();
 
-        let mut data = [0; 512];
-        match stream.read_exact(&mut data) {
-            Ok(_) => {
-              Ok((ProtocolType::String(from_utf8(&data).unwrap().to_string())).to_string())
-            },
-            Err(_) => {
-                Err("fail to get response")
+
+        let mut buffer = [0; 512];
+        let mut response_contents = Vec::new();
+        loop {
+             match stream.read(&mut buffer) {
+                 Ok(r) => {
+                     if r == 0 {
+                         break;
+                     }
+                     response_contents.extend_from_slice(&buffer[0..r]);
+                 }
+                 Err(_) => {
+                     if !response_contents.is_empty() {
+                         break;
+                     }
+                 }
+             }
+        }
+
+        let response_str = String::from_utf8(response_contents).unwrap();
+        let mut chars = response_str.chars();
+        return if let Some(mut parser) = ParserFactory::create(chars.next().unwrap()) {
+            for line in response_str.split("\r\n") {
+                println!("{}", line);
+                if parser.feed(&format!("{}\r\n", line)).unwrap() {
+                    break;
+                }
             }
+            Ok(parser.build().to_string().as_bytes().to_vec())
+        } else {
+            Err("Invalid RESP string")
         }
     },
     Err(_) => {
